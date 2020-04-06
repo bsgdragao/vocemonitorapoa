5565d60a517384223ac5c6f677d975d2f0f204a1
Bruno Guerra <bsgdragao@gmail.com>
2020-04-06 02:55:28 -0300

    Inserido Login com Facebook

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:vocemonitorapoa/screens/login_com_facebook.dart';
 import 'package:vocemonitorapoa/screens/login_email_page.dart';
 import 'package:vocemonitorapoa/screens/perfil_com_google_page.dart';
 import 'package:vocemonitorapoa/tasks/auth.dart';
@@ -130,11 +131,11 @@ class _GoogleSignAppState extends State<GoogleSignApp> {
                             ),
                           ],),
                         onPressed: () {
-//                          Navigator.push(context,
-//                            MaterialPageRoute(builder: (context) =>
-//                            new EmailLogin(),
-//                            ),
-//                          );
+                          Navigator.push(context,
+                            MaterialPageRoute(builder: (context) =>
+                            new LoginComFacebook(),
+                            ),
+                          );
                         },
                       ),
                     )
